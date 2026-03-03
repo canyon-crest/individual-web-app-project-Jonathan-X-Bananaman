efa7275e60db7d4fef303235dc0913c3a9c64db0
Jonathan-X-Bananaman <xuj4454@my.sduhsd.net>
2026-03-03 21:53:40 +0000

    interactions not working

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // add javascript here
+document.getElementById("show-facts").addEventListener("click", ()=>{
+    document.getElementById("facts").toggleAttribute("hidden");
+})
+
+document.getElementById("add-recipe").addEventListener("click", ()=>{
+    let lll=document.getElementById("recipe-input");
+    let ppp=document.getElementById("recipe-list");
+    if(lll.value.trim()!==""){
+        const newItem = document.createElement("li");
+        newItem.textContent = lll.value;
+        ppp.appendChild(newItem);
+        lll.value = "";
+    }
+})
