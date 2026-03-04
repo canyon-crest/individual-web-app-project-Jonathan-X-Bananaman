a9d65d95c61fab138e4b4d678a794e48a181edf4
Jonathan-X-Bananaman <xuj4454@my.sduhsd.net>
2026-03-04 20:01:45 +0000

    make button work

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // add javascript here
-document.getElementById("show-facts").addEventListener("click", ()=>{
+document.getElementById("show-facts")?.addEventListener("click", ()=>{
     document.getElementById("facts").toggleAttribute("hidden");
 })
 
-document.getElementById("add-recipe").addEventListener("click", ()=>{
-    let lll=document.getElementById("recipe-input");
-    let ppp=document.getElementById("recipe-list");
-    if(lll.value.trim()!==""){
+document.getElementById("add-recipe")?.addEventListener("click", ()=>{
+    let input=document.getElementById("recipe-input");
+    let list=document.getElementById("recipe-list");
+    if(input.value.trim()!==""){
         const newItem = document.createElement("li");
-        newItem.textContent = lll.value;
-        ppp.appendChild(newItem);
-        lll.value = "";
+        newItem.textContent = input.value;
+        list.appendChild(newItem);
+        input.value = "";
     }
 })
